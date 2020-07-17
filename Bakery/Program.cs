256b7749e814704286547028b8dc1f6c83642536
Ben White <bwhite2140@outlook.com>
2020-07-17 11:49:10 -0700

    add user interface for the bakery

--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -1,9 +1,30 @@
 using System;
+using Bakery.Models;
 
-class Program
+public class Program
 {
-  static void Main()
+  public static void Main()
   {
-
+    Console.WriteLine("Hello! Welcome to Happy Little Bakery!");
+    Console.WriteLine("Bread: $5"); 
+    Console.WriteLine("Pastries: $2");
+    Console.WriteLine("Deals: Buy two loaves get one free, Buy two pastries get the third $1 off");
+    Console.WriteLine("Would you like to buy something? (y or n)");
+    string userInput = Console.ReadLine();
+    userInput.ToLower();
+    if (userInput == "y")
+    {
+      Console.WriteLine("What would you like to buy? Enter amount of bread then pastries:");
+      int bread = int.Parse(Console.ReadLine());
+      int pastries = int.Parse(Console.ReadLine());
+      Bread breadOrder = new Bread(bread);
+      Pastry pastryOrder = new Pastry(pastries);
+      breadOrder.BreadDeal();
+      pastryOrder.PastryDeal();
+      Console.WriteLine("You bought " + breadOrder.Quantity + " loaves of bread for $" + 
+      breadOrder.TotalPrice + " and " + pastryOrder.Quantity + " pastries for $" + pastryOrder.TotalPrice);
+      Console.WriteLine("Your total is $" + (breadOrder.TotalPrice + pastryOrder.TotalPrice));
+    }
+    Console.WriteLine("Thank you for visiting Happy Little Bakery! Please come again!");
   }
 }
