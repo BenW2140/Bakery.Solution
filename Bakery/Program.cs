4ccf630a513587eae9a9474bcce71fbceffc3f63
Ben White <bwhite2140@outlook.com>
2020-07-17 13:54:16 -0700

    add validation to make sure the user entered something

--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -5,20 +5,34 @@ public class Program
 {
   public static void Main()
   {
+    Console.WriteLine("-------------------------------------------------------------------------");
     Console.WriteLine("Hello! Welcome to Happy Little Bakery!");
     Console.WriteLine("Bread: $5"); 
     Console.WriteLine("Pastries: $2");
     Console.WriteLine("Deals: Buy two loaves get one free, Buy two pastries get the third $1 off");
+    Console.WriteLine("-------------------------------------------------------------------------");
     Console.WriteLine("Would you like to buy something? (y or n)");
     string userInput = Console.ReadLine();
     userInput.ToLower();
     if (userInput == "y")
     {
       Console.WriteLine("What would you like to buy? Enter amount of bread then pastries:");
-      int bread = int.Parse(Console.ReadLine());
-      int pastries = int.Parse(Console.ReadLine());
-      Bread breadOrder = new Bread(bread);
-      Pastry pastryOrder = new Pastry(pastries);
+      string bread = Console.ReadLine();
+      string pastries = Console.ReadLine();
+      while (bread == "" || pastries == "")
+      {
+        Console.WriteLine("If you changed your mind about buying something enter 0, otherwise please enter a positive integer:");
+        bread = Console.ReadLine();
+        pastries = Console.ReadLine();
+        // while (numBread < 0 || numPastries < 0)
+        // {
+
+        // }
+      }
+      int numBread = int.Parse(bread);
+      int numPastries = int.Parse(pastries);
+      Bread breadOrder = new Bread(numBread);
+      Pastry pastryOrder = new Pastry(numPastries);
       breadOrder.BreadDeal();
       pastryOrder.PastryDeal();
       Console.WriteLine("You bought " + breadOrder.Quantity + " loaves of bread for $" + 
